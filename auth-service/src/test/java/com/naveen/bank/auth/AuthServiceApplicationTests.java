0f75f3455e51c86661cb24c33320e07d445a38fc
Naveen <naveenramavath133@gmail.com>
2026-07-02 16:41:03 +0530

    added auth service

---
+++ auth-service/src/test/java/com/naveen/bank/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.bank.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
